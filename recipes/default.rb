7b528f7c8ab726dc36bb3f5794a15b749c3e1cbe
Bart Ailey <bart.ailey@eaglegenomics.com>
2015-07-08 17:29:40 +0100

    end ot day commit, not working

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,20 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
+##########################################################
+
+include_recipe 'build-essential'
+include_recipe 'java'
+
+##########################################################
+# here for use by serverspec
+
+##########################################################
+
+remote_file "#{Chef::Config[:file_cache_path]}/#{node['TrimGalore']['filename']}" do
+  source node['TrimGalore']['url']
+  action :create_if_missing
+end
+
+##########################################################
+##########################################################
