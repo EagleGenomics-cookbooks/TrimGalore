5bc5f3c03e38d8e9689ef665de5343e0e8c8ce97
Bart Ailey <bart.ailey@eaglegenomics.com>
2015-07-09 14:53:14 +0100

    added FastQC and cutadapt cookbook dependencies. Not tested

--- Berksfile
+++ Berksfile
@@ -1,5 +1,7 @@
 source 'https://supermarket.chef.io'
 
 cookbook 'magic_shell', '~> 1.0.0'
+cookbook "FastQC", git: "https://github.com/EagleGenomics-cookbooks/FastQC.git"
+cookbook "cutadapt", git: "https://github.com/EagleGenomics-cookbooks/cutadapt.git"
 
 metadata
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,21 +1,33 @@
 #
-# Cookbook Name:: templateCookbook
+# Cookbook Name:: TrimGalore
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 ##########################################################
 
 include_recipe 'build-essential'
-include_recipe 'java'
+include_recipe 'FastQC' 
+include_recipe 'cutadapt' 
 
 ##########################################################
 # here for use by serverspec
+
+
+##########################################################
+
+package ['unzip'] do
+  action :install
+end
 
 ##########################################################
 
 remote_file "#{Chef::Config[:file_cache_path]}/#{node['TrimGalore']['filename']}" do
   source node['TrimGalore']['url']
   action :create_if_missing
+end
+
+execute "unzip #{Chef::Config[:file_cache_path]}/#{node['TrimGalore']['filename']} -d #{node['TrimGalore']['install_dir']}" do
+  not_if { ::File.exist?("#{node['TrimGalore']['install_dir']}/trim_galore_zip/trim_galore") }
 end
 
 ##########################################################
