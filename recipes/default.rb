e3ea957b24922eda22bf7d8247317033633a462c
Bart Ailey <bart.ailey@eaglegenomics.com>
2015-07-10 11:50:19 +0100

    Serverspec testing complete.

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,6 +12,13 @@ include_recipe 'cutadapt'
 ##########################################################
 # here for use by serverspec
 
+magic_shell_environment 'TRIM_GALORE_VERSION' do
+  value node['TrimGalore']['version']
+end
+
+magic_shell_environment 'TRIM_GALORE_INSTALL_DIR' do
+  value node['TrimGalore']['install_dir']
+end
 
 ##########################################################
 
@@ -28,6 +35,10 @@ end
 
 execute "unzip #{Chef::Config[:file_cache_path]}/#{node['TrimGalore']['filename']} -d #{node['TrimGalore']['install_dir']}" do
   not_if { ::File.exist?("#{node['TrimGalore']['install_dir']}/trim_galore_zip/trim_galore") }
+end
+
+link "#{node['TrimGalore']['bin_path']}/trim_galore" do
+  to "#{node['TrimGalore']['install_dir']}/trim_galore_zip/trim_galore"
 end
 
 ##########################################################
--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -3,3 +3,34 @@ require_relative './spec_helper'
 
 # Required by serverspec
 set :backend, :exec
+
+
+describe command('which fastqc') do
+  its(:exit_status) { should eq 0 }
+end
+
+describe command('which cutadapt') do
+  its(:exit_status) { should eq 0 }
+end
+
+describe file("#{ENV['TRIM_GALORE_INSTALL_DIR']}/trim_galore_zip") do
+  it { should be_directory }
+end
+
+describe file("#{ENV['TRIM_GALORE_INSTALL_DIR']}/trim_galore_zip/trim_galore") do
+  it { should be_file }
+  it { should be_executable }
+end
+
+describe command('which trim_galore') do
+  its(:exit_status) { should eq 0 }
+end
+
+versionEdited = ENV['TRIM_GALORE_VERSION'].gsub(/v/, '')
+
+describe command('trim_galore -version') do
+  its(:exit_status) { should eq 0 }
+  its(:stdout) { should contain versionEdited }
+end
+
+
