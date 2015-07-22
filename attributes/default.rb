00a7ef39ebb19dcf19918375c2ab5fa9bf068377
Bart Ailey <bart.ailey@eaglegenomics.com>
2015-07-22 14:02:07 +0100

    added .rubocop.yml, .kitchen.yml

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default['TrimGalore']['version'] = 'v0.4.0'
-default['TrimGalore']['filename'] = 'trim_galore_' + default['TrimGalore']['version'] + '.zip';
+default['TrimGalore']['filename'] = 'trim_galore_' + default['TrimGalore']['version'] + '.zip'
 default['TrimGalore']['url'] = 'http://www.bioinformatics.babraham.ac.uk/projects/trim_galore/' + default['TrimGalore']['filename']
 default['TrimGalore']['install_dir'] = '/usr/local/'
 default['TrimGalore']['bin_path'] = '/usr/local/bin'
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -5,5 +5,3 @@
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
 require 'spec_helper'
-
-
