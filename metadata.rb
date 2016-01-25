559fbe76a83244bea055c754252ec9e9e1b9b800
Daniel Barrell <daniel.barrell@eaglegenomics.com>
2016-01-25 14:13:57 +0000

    License, maintainers, description etc

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 name 'TrimGalore'
-maintainer 'The Authors'
+maintainer 'Eagle Genomics'
 maintainer_email 'chef@eaglegenomics.com'
-license 'all_rights'
+license 'apache2'
 description 'Installs/Configures TrimGalore'
-long_description 'Installs/Configures TrimGalore'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.2'
 
 depends 'build-essential'
