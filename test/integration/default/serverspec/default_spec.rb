b7c75a1154cbb015e879d3af45be27470859ba92
GreetDB <grdbaets@gmail.com>
2017-02-15 09:24:32 +0000

    hardcode path of TrimGalore / change to_s

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -12,11 +12,11 @@ describe command('which cutadapt') do
   its(:exit_status) { should eq 0 }
 end
 
-describe file("#{ENV['TRIM_GALORE_INSTALL_DIR']}/trim_galore_zip") do
+describe file('/usr/local/trim_galore_zip') do
   it { should be_directory }
 end
 
-describe file("#{ENV['TRIM_GALORE_INSTALL_DIR']}/trim_galore_zip/trim_galore") do
+describe file('/usr/local/trim_galore_zip/trim_galore') do
   it { should be_file }
   it { should be_executable }
 end
@@ -25,7 +25,7 @@ describe command('which trim_galore') do
   its(:exit_status) { should eq 0 }
 end
 
-version_edited = ENV['TRIM_GALORE_VERSION'].delete('v')
+version_edited = ENV['TRIM_GALORE_VERSION'].to_s.delete! 'v'
 
 describe command('trim_galore -version') do
   its(:exit_status) { should eq 0 }
